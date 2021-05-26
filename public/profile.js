20848097919cca6f044423423f15b92111a2ee7c
Josefina <jfernandez7@uc.cl>
2021-05-26 13:05:04 -0400

    ready

--- public/profile.js
+++ public/profile.js
@@ -1,4 +1,6 @@
-const user = {"id":3,"name":"Jose","email":"jfernandez7@uc.cl"}
+const current_user = {"id":3,"name":"Jose","email":"jfernandez7@uc.cl"}
+const profile = document.querySelector('main');
+
 //const user = window.localStorage.getItem('user');
 
 if ('serviceWorker' in navigator) {
@@ -22,19 +24,33 @@ window.addEventListener('online', () => updateProfile());
 
 async function updateProfile() {
   profile.innerHTML = '';
-  // const response = await fetch(`http://localhost:3000/profile/${user.id}`);
-  const response = await fetch(`https://server-web-avanzado.free.beeceptor.com/profile/id=3`);
+
+  const response = await fetch(`https://pwitter-19436-default-rtdb.firebaseio.com/users.json`);
   console.log(response)
   const json = await response.json();
-  feed.innerHTML =
-    createProfile(json)
+  console.log(json)
+  const filteredUser = json.filter(usuario => usuario.email === current_user.email);
+  console.log(filteredUser)
+  const response2 = await fetch(`https://pwitter-19436-default-rtdb.firebaseio.com/pweets.json`);
+  const json2 = await response2.json();
+
+  const filteredPweets = json2.filter(pweet => pweet.email === current_user.email);
+  console.log(filteredPweets)
+  const profileData = {
+    user: filteredUser,
+    pweets: filteredPweets
+  }
+  console.log("profiledata:", profileData)
+  profile.innerHTML =
+  
+    createProfile(profileData)
 }
 
 function createProfile(info) {
     const myData = `
     <div class="personalInfo">
-        <h2>Name: ${info.name}</h2>
-        <h3>Email: ${info.email}</p>
+        <h2>Name: ${info.user[0].name}</h2>
+        <h3>Email: ${info.user[0].email}</p>
         <h3>Published pweets: </p>
 
     </div>
